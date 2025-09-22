f8c6e80a40229d290c1ca7fa47dfafb1f4ffe785
Irfan Syed <isyed@Mac.gateway>
2025-09-22 10:09:26 +1000

    Implement getArnieQuotes to fetch and format quotes from provided URLs

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,27 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+	// TODO: Implement this function.
+	// Creating an array of promises using httpGet for each URL in the urls array.
+	const promises = urls.map(async (url) => {
+		try {
+			const response = await httpGet(url);
+			const data = JSON.parse(response.body);
+			if (response.status === 200) {
+				return { 'Arnie Quote': data.message };
+			} else {
+				return { FAILURE: data.message };
+			}
+		} catch (error) {
+			return { FAILURE: error.message };
+		}
+	});
+
+	// Using Promise.all to wait for all promises to resolve.
+	const result = await Promise.all(promises);
+	return result;
 };
 
 module.exports = {
-  getArnieQuotes,
+	getArnieQuotes,
 };
